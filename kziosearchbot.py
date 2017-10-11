0b5dab7c292846290ad66d54dec81e2409969012
М С. Абакумов <abakumovMS@p4udp.local>
2017-10-11 16:24:05 +0300

    eleventh commit

--- kziosearchbot.py
+++ kziosearchbot.py
@@ -1,5 +1,22 @@
-import telepot
-token = '423472303:AAFLD7h1pPYAYVqe6CauuOwcnkDwKzihnyM'
-TelegramBot = telepot.Bot(token)
-print TelegramBot.getMe()
+# -*- coding: utf-8 -*-
 
+import telebot
+
+API_TOKEN = '423472303:AAFLD7h1pPYAYVqe6CauuOwcnkDwKzihnyM'
+
+bot = telebot.TeleBot(API_TOKEN)
+bot.remove_webhook()
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """
+Привет! 
+Я тестовый бот, и умею я пока немного. Напиши мне и я отвечу!
+""")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
