04a12eab02f9cbac41733cde04253011f29407f9
KamidzuMSK <kamidzu-msk@yandex.ru>
2017-10-30 21:38:25 +0300

    home commit

--- kziosearchbot.py
+++ kziosearchbot.py
@@ -1,24 +1,70 @@
-# -*- coding: utf-8 -*-
+import requests  
+import datetime
 
-import telebot
-import config
-import requests
+class BotHandler:
+
+    def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot423472303:AAFLD7h1pPYAYVqe6CauuOwcnkDwKzihnyM/".format(token)
+
+    def get_updates(self, offset=None, timeout=30):
+        method = 'getUpdates'
+        params = {'timeout': timeout, 'offset': offset}
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()['result']
+        return result_json
+
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
+
+    def get_last_update(self):
+        get_result = self.get_updates()
+
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+        else:
+            last_update = get_result[len(get_result)]
+
+        return last_update
+greet_bot = BotHandler(token)  
+greetings = ('Здравствуйте', 'Привет', 'Приветствую')  
+now = datetime.datetime.now()
 
 
-bot = telebot.TeleBot(config.token)
+def main():  
+    new_offset = None
+    today = now.day
+    hour = now.hour
 
+    while True:
+        greet_bot.get_updates(new_offset)
 
-@bot.message_handler(commands=["start"]) # РћР±СЂР°Р±РѕС‚РєР° /start
-def handle_start(message):
-    bot.send_message(message.from_user.id, 'Hi! \nMy friend')
+        last_update = greet_bot.get_last_update()
 
-@bot.message_handler(content_types=["text"])
-def handle_t(message):
-    if message.text[:7] == "РџРѕРіРѕРґР° " or message.text[:7] == "РїРѕРіРѕРґР° " :
-            city = message.text[7:]
-            r = requests.get('http://api.openweathermap.org/data/2.5/weather?&units=metric&q=%s&appid=0c9f3c052f1d81b7062750ff0926f345<img src="https://habrastorage.org/files/8fa/5f5/313/8fa5f5313b37438eb250b22cf041f2dd.png" alt="image"/>' % (city))
-            data = r.json()
-            temp = data["main"]["temp"]
-            bot.send_message(message.chat.id, 'РўРµРјРїРµСЂР°С‚СѓСЂР° РІ ', city, ': ',temp , 'В°C')
+        last_update_id = last_update['update_id']
+        last_chat_text = last_update['message']['text']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['chat']['first_name']
 
-bot.polling(none_stop=True, interval=0)
+        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+            greet_bot.send_message(last_chat_id, 'Доброе утро,  {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+            greet_bot.send_message(last_chat_id, 'Добрый вечер,  {}'.format(last_chat_name))
+            today += 1
+
+        new_offset = last_update_id + 1
+
+if __name__ == '__main__':  
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
